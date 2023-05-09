998e2dd839eb94e320310acaf3acb7f2c380fdc0
Pari-Malam <apid9755@gmail.com>
2023-05-09 10:00:49 +0800

    Update shell-scanner.py

--- shell-scanner.py
+++ shell-scanner.py
@@ -74,9 +74,6 @@ with open('Wordlist/Shell-Strings.txt', 'r') as f1, open('Wordlist/Shell-Strings
 
 headers = {
     'User-Agent': random.choice(user_agents),
-    'Content-type': '*/*',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
     'Connection': 'keep-alive'
 }
 
